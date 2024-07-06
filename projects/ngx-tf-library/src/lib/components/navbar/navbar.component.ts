e06ffe6a0bf4c09df44938527a31f3326946df46
CAIsoul <caisoul92@gmail.com>
2024-07-06 16:01:28 +0800

    Implement toggle for navbar

--- projects/ngx-tf-library/src/lib/components/navbar/navbar.component.ts
+++ projects/ngx-tf-library/src/lib/components/navbar/navbar.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.less'
+  styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  @Input() isExpanded: boolean = false;
+  @Input() prodBannerUrl: string = "";
 
+  constructor() {
+  }
+
+  toggleNavbar(isToExpand?: boolean): void {
+
+    if (typeof isToExpand !== 'boolean') {
+      isToExpand = !this.isExpanded;
+    }
+
+    this.isExpanded = isToExpand;
+  }
 }
